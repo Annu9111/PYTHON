8e733a47f85e4ec249c41091c018710861eddd8d
Annu9111 <aksoni911190@gmail.com>
2025-12-17 14:06:09 +0530

    modules and pip

---
+++ modules_and_pip.py
@@ -0,0 +1,7 @@
+import pandas
+import hashlib     #it is python builtin module don't need to install,ready to use module
+import tensorflow   #it is an external module, first need to install it by writitng pip install tensorflow then import it
+print("hii")
+
+
+# Python modules help organize code and add functionality. They are categorized into built-in modules, which come with the Python interpreter, and external modules, which are developed by the community and must be installed separately
