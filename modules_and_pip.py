51dc99d75b9126ac3526c3fd805cf167bd4043db
Annu9111 <aksoni911190@gmail.com>
2025-12-17 14:11:37 +0530

    modules and pip

--- modules_and_pip.py
+++ modules_and_pip.py
@@ -5,3 +5,5 @@ print("hii")
 
 
 # Python modules help organize code and add functionality. They are categorized into built-in modules, which come with the Python interpreter, and external modules, which are developed by the community and must be installed separately
+
+# PIP (which recursively stands for "Pip Installs Packages") is the standard package manager for Python. It is a command-line tool that allows you to install, manage, and uninstall software packages (libraries and modules) that are not part of the standard python library
