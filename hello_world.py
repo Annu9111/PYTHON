88265618c6ad20e15fc1e5c5c102402c8372a588
Annu9111 <aksoni911190@gmail.com>
2025-12-18 08:09:29 +0530

    hellow world

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+print("hellow world!!")
+print(2+3)
+print("hellow",8)
+print(89*34)
+a="ram"
+age="23"
+print(a,"is",age,"old")
+# The print() function in Python is a built-in function used to display output to the console or other standard output devices. It is one of the most commonly used functions for debugging and providing user feedback. 
+# Basic Usage
