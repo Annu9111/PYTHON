8f7185c1180eeceb86425b91faffec89e2516668
Annu9111 <aksoni911190@gmail.com>
2025-12-19 13:00:12 +0530

    comments,escape chars,print

---
+++ commit_escape_print.py
@@ -0,0 +1,23 @@
+#this is a single line comment
+''' this is a multiline commit
+write comments in many lines'''
+# ----------------------------------------------------------------------------------
+
+#escape sequence
+'''Python escape sequences are combinations of characters starting with a 
+backslash (\) that are used to insert special, non-printable,
+or otherwise illegal characters into a string'''
+
+print("this is annu\'s code ")    #\' for single quote
+print("my name is \"annu\"")       #\" for double quote
+print("hello eneryone!! \n welcome to my channel")     #\n  for next line
+print("annu\tsoni")   #\t for tab space
+# etc
+
+# ------------------------------------------------------------------------------------------
+# parameters of print statement
+print("annu",3,9+8)    #you can print multiple values at ones
+print("a","b","c","d",sep="-")     #defines what comes between multiple values
+print("hello",end=" ")      #defines what is printed at the end
+print("world")
+# etc
