92d1e6c91accac3e3b89db8971e6e7ce7595472f
Annu9111 <aksoni911190@gmail.com>
2025-12-20 10:03:17 +0530

    variables and datatype

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+"annu"     #a is a veriable
+# print(a)
+# b=123455      #b is a variable
+# print(b)
---
+++ variables_and_datatype.py
@@ -0,0 +1,42 @@
+# variables: variables are like a cointainers which holds different types of data, it is like a placeholder in memory which holds data
+
+a="annu"     #a is a veriable
+print(a)
+b=123455      #b is a variable
+print(b)
+# ------------------------------------------------------------
+# datatypes=it specify the type of value a variable holds.this is required in programing for various operation without getting error
+
+a="annu"   #datatype=string
+print(type(a))
+
+b=345    #int
+print(type(b))
+
+c=True    #bool
+print(type(c))
+
+d={}   #dict
+print(type(d))
+
+e=set()   #set
+print(type(e))
+
+f=[]    #list
+print(type(f))
+
+g=()    #tuple
+print(type(g))
+
+h=234.5   #float
+print(type(h))
+
+i=None   #nonetype
+print(type(i))
+
+j=3+4j     #complex
+print(type(j))
+
+# etc
+# also for concatination we need a same type of datatype
+# also we get the type of variable by using type function eg:
