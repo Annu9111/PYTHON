ff7ff0633161d7d486c511496627f2829ab43252
Annu9111 <aksoni911190@gmail.com>
2025-12-24 17:23:49 +0530

    conditional statement

---
+++ conditional_statements.py
@@ -0,0 +1,17 @@
+a=int(input("enter your age: "))
+print("your age is ",a)
+
+if a<18:
+    print("you cannot drive")
+else:
+    print("you can drive")     
+
+# --------------------------------------------------------------------------------
+
+b=int(input("Enter number: "))
+if b<0:
+    print("negative number")
+elif b==0:
+    print("Zero")
+else:
+    print("positive number")        
