97d7c0ab04005ff100f12dfc9c53c6eb908c9fbd
Annu9111 <aksoni911190@gmail.com>
2026-01-02 20:45:37 +0530

    recursion

---
+++ recursion.py
@@ -0,0 +1,14 @@
+# recursion is a process of defining something in terms of itself
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    return n*factorial(n-1)
+
+print(factorial(5))
+
+def fibonacci(n):
+    if n<=1:
+        return n
+    return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(6))
