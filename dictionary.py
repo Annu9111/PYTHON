e3b72208b6397105d594d104a76241fb05d60e46
Annu9111 <aksoni911190@gmail.com>
2026-01-06 21:12:40 +0530

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,5 @@
-# dictionaries are ordered(python 3.7 onwards) pairs of keys values, with unique kyes 
-# initialising dictionary
+# dictionaries are ordered(python 3.7 onwards) pairs of keys values, with unique keys 
+# initialising dictionary  
 a={}
 print(type(a))
 
@@ -18,6 +18,16 @@ print(info.items())  #return all the keys values pairs
 for key,value in info.items():   #it will unpack the tuple
     print(f"her {key} is {value}")
     
+#dictionary method
+d1={1:23,2:45,3:56,4:67,5:89,6:76}
+d2={'a':"A","b":"B","c":"C"}
+d1.update(d2)  #it will add d2 in d1
+print((d1))
+d2.clear() #it will clear all the items from d1
+d1.pop(1)  #it will remove key value pair of 1
+d1.popitem(6)   #it will remove last element
+del d1[1]   #it will remove key value pair of 1
+       
 
 
 
