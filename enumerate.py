2acb35cca2c502c15f12f485d698a16616885ef9
Annu9111 <aksoni911190@gmail.com>
2026-01-07 20:40:21 +0530

    enumerate

---
+++ enumerate.py
@@ -0,0 +1,9 @@
+# Enumerate:it gives index and value of the sequence at the name time 
+fruits=["apple","banana","grapes","oranges","mango"]
+for index,fruit in enumerate(fruits):
+    print(index,fruit)
+    
+names=["priya","annu","riya","tina"]
+for index,name in enumerate(names,start=1):
+    print(index,name)     
+    
