4bda6334244a1079fc63d0b1e16c162f6b8107e7
Annu9111 <aksoni911190@gmail.com>
2026-01-23 07:51:08 +0530

    static method

---
+++ static_method.py
@@ -0,0 +1,17 @@
+#static method = these are the methods that belong to a class rather than an instance of the class.They are define under @staticmethod decorator and do not have accessto the instance of the class.
+
+class Maths:
+    def __init__(self,num):
+        self.num=num
+        
+    def addtonum(self,n):
+        self.num=self.num+n
+        
+    @staticmethod
+    def add(a,b):
+        return a+b
+    
+    
+res=Maths.add(2,5)
+print(res)
+            
