7776ef9b13818a8cc0691aca8856954edaf7bd87
Annu9111 <aksoni911190@gmail.com>
2026-02-05 06:11:17 +0530

    mutilevel inheritance

---
+++ multilevel_inheritance.py
@@ -0,0 +1,29 @@
+class Animal:
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+        
+    def show_details(self):
+        print(f"name:{self.name} ,Species:{self.species}")
+        
+class Dog(Animal):
+    def __init__(self,name,breed):
+        Animal.__init__(self,name,species="dog")
+        self.breed=breed
+        
+    def show_details(self):
+        Animal.show_details(self)
+        print(f"Breed:{self.breed}")       
+        
+class Goldenretriver(Dog):
+    def __init__(self,name,color):
+        Dog.__init__(self,name,breed="Goldenretriver")
+        self.color=color
+    
+    def show_details(self):
+        Dog.show_details(self)
+        print(f"color:{self.color}")
+        
+o=Goldenretriver("tommy","brown")
+o.show_details()        
+                            
