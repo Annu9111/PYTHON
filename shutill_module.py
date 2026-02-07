f05e705d21a43a7039413f739d1cea2621e6f7d0
Annu9111 <aksoni911190@gmail.com>
2026-02-07 16:09:42 +0530

    shutil module

---
+++ shutill_module.py
@@ -0,0 +1,7 @@
+import shutil
+shutil.copy("main.txt","main2.txt")
+
+shutil.copytree(".tutorial","mytutorial")   #copy all the files inside folder
+
+shutil.move(".tutorial/file.file","file.file")
+
